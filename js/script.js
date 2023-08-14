6789cfb1b589ee5f24ea83455e869635febd7713
Avenir Prokopenko <121766514+AvenirProkopenko@users.noreply.github.com>
2023-08-14 13:19:22 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// Получаем ссылку на видео-элемент
+const introVideo = document.querySelector('#intro-screen video');
+
+// Задаем продолжительность видео-заставки (в миллисекундах)
+const introDuration = 7000;
+
+// После окончания видео-заставки перенаправляем на другой экран
+introVideo.addEventListener('ended', () => {
+   window.location.href = 'src/main.html'; // Замените на URL другого экрана
+});
+
+// Запускаем видео-заставку
+introVideo.play();
+
+// Устанавливаем таймер для перехода на другой экран
+setTimeout(() => {
+   window.location.href = 'src/main.html'; // Замените на URL другого экрана
+}, introDuration);
+
+
+const menuLinks = document.querySelectorAll('.menu-link');
+const contents = document.querySelectorAll('.content');
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const target = link.dataset.target;
+        contents.forEach(content => {
+            if (content.id === target) {
+                content.classList.remove('hidden');
+            } else {
+                content.classList.add('hidden');
+            }
+        });
+        menuLinks.forEach(menuLink => {
+            menuLink.classList.remove('active');
+        });
+        link.classList.add('active');
+    });
+});
+
+// Получение элементов модального окна и крестика
+const modal = document.getElementById('myModal');
+const modalContent = document.querySelector('.modal-content');
+const closeButton = document.querySelector('.close-button');
+
+// Открытие модального окна при нажатии на кнопку "ОТВЕТИТЬ"
+const replyButton = document.querySelector('.reply-button');
+replyButton.addEventListener('click', function() {
+  modal.style.display = 'block';
+});
+
+// Закрытие модального окна при нажатии на крестик
+closeButton.addEventListener('click', function() {
+  modal.style.display = 'none';
+});
+
+// Закрытие модального окна при щелчке вне окна
+window.addEventListener('click', function(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+
+
+
+
+
